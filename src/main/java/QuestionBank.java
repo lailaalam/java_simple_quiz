d0b2d8f70c508aed15ab052afc12b83bb3144609
lailaalam <alamlaila09@gmail.com>
2022-07-31 18:16:04 +0600

    changes in option showing

--- src/main/java/QuestionBank.java
+++ src/main/java/QuestionBank.java
@@ -12,7 +12,7 @@ import org.json.simple.parser.ParseException;
 public class QuestionBank {
     public static void main(String[] args) throws IOException, ParseException {
 
-        System.out.println("Please Enter a number 1 or 2:");
+        System.out.println("Please Enter a number 1 or 2 \n 1 : Add question\n 2 : Participate in a quiz");
         Scanner input = new Scanner(System.in);
         int userInput = input.nextInt();
         if (userInput == 1) {
